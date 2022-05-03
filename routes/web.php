27e62db34023903aa31a68b2b47a1d8843d7a6e1
Lam Pham <phamkhaclam90@gmail.com>
2022-05-03 12:30:56 +0700

    Update some changes for routing test

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('buy/{cookies}', function ($cookies) {
+    $wallet = Auth::user()->wallet;
+    Auth::user()->wallet = $wallet - $cookies * 1;
+    Auth::user()->save();
+    \Log::info('User ' . Auth::user()->email . ' have bought ' . $cookies . ' cookies'); // we need to log who ordered and how much
+
+    return 'Success, you have bought ' . $cookies . ' cookies!';
 });
